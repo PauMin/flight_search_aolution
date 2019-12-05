523ae0f8b7dc46ee8ac8deebbdfe61a8796f57ed
Paul Mintskovsky <pmintskovsky@gmail.com>
2019-12-05 23:31:36 +0200

    Initial commit

--- flight_search.py
+++ flight_search.py
@@ -4,84 +4,89 @@ import ijson
 import pandas as pd
 
 
-def get_best(f):
-    itinerary = pd.DataFrame(ijson.items(f, 'itinerary.item'))
+def find_optimal_combination(f):
+    # Get itinerary and fares to separate dictionaries
+    itinerary = [*ijson.items(f, 'itinerary.item')]
 
-    satisfactory = satisfactory_generator(f, itinerary)
-    variations = [{
-        'indexes': [fare['index']],
-        'routes': [fare['routes']],
-        'price': fare['price'],
-        'weight': fare['weight']
-    } for fare in next(satisfactory).iterrows()]
+    # Find variations
+    variation = get_variations(f, itinerary)
 
-    found = False
-
-    # while not found:
-        # ---
-    # fares = ijson.items(f, 'fares.item')
-    # route = routes[0]
-    # found = False
-    # no_show = [fare['fid'] for fare in fares if not all(route in routes for route in fare['routes'])]
-    #
-    # f.seek(0, 0)
-    #
-    # fares = ijson.items(f, 'fares.item')
-    # variations = [{
-    #     'indexes': [fare['fid']],
-    #     'routes': fare['routes'],
-    #     'price': fare['price'],
-    #     'weight': fare['price'] / len(fare['routes'])
-    # } for fare in fares if route in fare['routes'] and fare['fid'] not in no_show]
-    # variations = sorted(variations, key=lambda i: i['weight'])
-    #
-    # while not found:
-    #     f.seek(0, 0)
-    #     fares = ijson.items(f, 'fares.item')
-    #     route = [route for route in routes if route not in variations[0]['routes']][0]
-    #     satisfactory = [{
-    #         'current_index': fare['fid'],
-    #         'indexes': [fare['fid']],
-    #         'routes': fare['routes'],
-    #         'price': fare['price'],
-    #         'weight': variations[0]['price'] / len(variations[0]['routes'])
-    #     } for fare in fares if route in fare['routes'] and not any(route in variations[0]['routes'] for route in fare['routes']) and fare['fid'] not in no_show]
-    #     satisfactory = sorted(satisfactory, key=lambda i: i['weight'])
-    #
-    #     variations[0]['indexes'].extend(satisfactory[0]['indexes'])
-    #     variations[0]['routes'].extend(satisfactory[0]['routes'])
-    #     variations[0]['price'] += satisfactory[0]['price']
-    #     variations[0]['weight'] = variations[0]['price'] / len(variations[0]['routes'])
-    #
-    #     variations = sorted(variations, key=lambda i: i['weight'])
-    #
-    #     if len(variations[0]['routes']) == len(routes):
-    #         found = True
-
-    return variations[0]['indexes']
+    return variation['indexes']
 
 
-def satisfactory_generator(f, itinerary):
-    for route in itinerary.iterrows():
+def get_satisfactory_generator(f, itinerary):
+    visited = []
+    for route in itinerary:
         f.seek(0, 0)
         fares = ijson.items(f, 'fares.item')
-        satisfactory = pd.DataFrame([fare for fare in fares if route[1].to_dict() in fare['routes']])
-        satisfactory.drop(no_show_generator(satisfactory, itinerary), inplace=True)
-        satisfactory['weight'] = satisfactory.price / len(satisfactory.routes)
+        satisfactory = pd.DataFrame([fare for fare in fares if route in fare['routes']]).set_index('fid')
+
+        # Exclude visited fares
+        satisfactory.drop(visited, errors='ignore', inplace=True)
+
+        # Exclude no show fares
+        satisfactory.drop([i for i in no_show_generator(satisfactory, itinerary)], inplace=True)
+
+        # Put weights on fares and sort them
+        satisfactory['weight'] = [fare[1].price / len(fare[1].routes) for fare in satisfactory.iterrows()]
         satisfactory.sort_values(by=['weight'], inplace=True)
+
+        # Drop duplicate fares with higher weight
+        satisfactory["t_routes"] = satisfactory.routes.apply(lambda x: tuple(json.dumps(x)))
+        satisfactory.drop_duplicates(subset='t_routes', keep='first', inplace=True)
+        satisfactory.drop(['t_routes'], axis=1, inplace=True)
+
+        visited.extend(satisfactory.index)
+
         yield satisfactory
 
 
 def no_show_generator(df, itinerary):
     for i, fare in df.iterrows():
-        result = pd.Series([route in itinerary.T.to_dict().values() for route in fare['routes']]).all()
+        result = pd.Series([route in itinerary for route in fare['routes']]).all()
         if ~result:
             yield i
 
 
+def get_variations(f, itinerary):
+    variations = []
+    found = False
+
+    satisfactory_generator = get_satisfactory_generator(f, itinerary)
+
+    satisfactory = next(satisfactory_generator)
+
+    variations.extend(
+        [{
+            'indexes': [fare[0]],
+            'routes': fare[1].routes,
+            'price': fare[1].price,
+            'weight': fare[1].weight
+        } for fare in satisfactory.iterrows()]
+    )
+
+    while not found:
+        satisfactory = next(satisfactory_generator)
+        fill = satisfactory.iloc[0]
+        i_s = [i for i, variation in enumerate(variations) if not any(route in variation['routes'] for route in fill.routes)]
+
+        for i in i_s:
+            variations[i]['indexes'].append(satisfactory.first_valid_index())
+            variations[i]['routes'].extend(fill.routes)
+            variations[i]['price'] += fill.price
+            variations[i]['weight'] = variations[0]['price'] / len(variations[0]['routes'])
+
+        variations = sorted(variations, key=lambda i: i['weight'])
+
+        if len(variations[0]['routes']) == len(itinerary):
+            found = True
+
+    return variations[0]
+
+
 def main():
     with open(os.environ["DATA_FILE"], "r") as f:
-        answer = get_best(f)
+        answer = find_optimal_combination(f)
 
     with open(os.environ["RESULT_FILE"], 'w') as f:
         f.write(json.dumps(answer))
